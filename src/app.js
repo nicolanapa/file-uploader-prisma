b845684fc5475e09337f81a0d4e161885520f7ac
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-02-26 21:35:11 +0100

    feat: Updated session use and added Session model to schema

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ import url from "url";
 import path from "path";
 import session from "express-session";
 import passport from "passport";
-// import { PrismaClient, Prisma } from "@prisma/client";
+import { PrismaSessionStore } from "@quixo3/prisma-session-store";
+import { PrismaClient } from "@prisma/client";
 
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -15,13 +16,25 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(session({ secret: process.env.SECRET_SESSION, resave: false, saveUninitialized: false }));
+app.use(
+    session({
+        store: new PrismaSessionStore(new PrismaClient(), {
+            checkPeriod: 2 * 60 * 1000,
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }),
+        secret: process.env.SECRET_SESSION,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        },
+    }),
+);
 app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname + "/styles")));
 app.use(express.static(path.join(__dirname + "/scripts")));
-
-// const prisma = new PrismaClient();
 
 app.get("/", (req, res) => {
     res.send();
