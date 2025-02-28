1a3a623278a461d4d627c67cf22301febaa55b39
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-02-28 22:28:34 +0100

    feat: Added query to prisma to add a file info to POST /

--- src/routes/uploadRouter.js
+++ src/routes/uploadRouter.js
@@ -1,13 +1,28 @@
 import { Router } from "express";
 import multer from "multer";
+import { prisma } from "../app.js";
 
 const upload = multer({ dest: "./drive" });
 
 const uploadRouter = new Router();
 
-uploadRouter.post("/", upload.single("uploadedFile"), (req, res) => {
-    console.log(req.body);
+uploadRouter.post("/", upload.single("uploadedFile"), async (req, res) => {
+    // console.log(req.body);
     console.log(req.file);
+
+    const file = await prisma.file.create({
+        data: {
+            filename: req.file.filename,
+            FileInformation: {
+                create: {
+                    originalFilename: req.file.originalname,
+                    sizeInBytes: req.file.size,
+                },
+            },
+        },
+    });
+
+    console.log(file);
 });
 
 export default uploadRouter;
