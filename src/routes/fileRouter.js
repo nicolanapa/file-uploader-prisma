d96a6e46feec07c32838c68114ae5d7cc4c5fca2
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-03-10 20:46:35 +0100

    feat: POST /:uniqueIdentifier/rename to rename a file with express validator

--- src/routes/fileRouter.js
+++ src/routes/fileRouter.js
@@ -1,12 +1,25 @@
 import { Router } from "express";
 import { prisma } from "../app.js";
+import { body, validationResult } from "express-validator";
+import * as fs from "node:fs/promises";
+
+const validateFileName = [
+    body("fileName")
+        .trim()
+        .notEmpty()
+        .withMessage("File Name must not be empty")
+        .not()
+        .contains("/")
+        .withMessage("No / symbols are allowed")
+        .not(),
+];
 
 const fileRouter = Router();
 
 fileRouter.get("/:uniqueIdentifier", async (req, res) => {
     if (!req.isAuthenticated()) {
         console.log("Not authenticated");
-        
+
         return res.redirect("/login");
     }
 
@@ -31,5 +44,50 @@ fileRouter.get("/:uniqueIdentifier", async (req, res) => {
         file: file ? file : {},
     });
 });
+
+fileRouter.post(
+    "/:uniqueIdentifier/rename",
+    validateFileName,
+    async (req, res) => {
+        if (!req.isAuthenticated()) {
+            console.log("Not authenticated");
+
+            return res.redirect("/login");
+        }
+
+        const errors = validationResult(req);
+
+        console.log(errors.array());
+
+        if (!errors.isEmpty()) {
+            return res.status(400).redirect("/");
+        }
+
+        const file = await prisma.file.findUnique({
+            where: {
+                filename: req.params.uniqueIdentifier,
+            },
+            include: {
+                fileInformation: true,
+            },
+        });
+
+        if (file !== null) {
+            console.log(file);
+
+            await prisma.fileInformation.update({
+                where: {
+                    fileId: file.id,
+                },
+                include: {
+                    file: true,
+                },
+                data: {
+                    originalFilename: req.body.fileName,
+                },
+            });
+        }
+    },
+);
 
 export default fileRouter;
