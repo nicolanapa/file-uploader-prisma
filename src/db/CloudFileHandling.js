68897eefa9c08c1d71739f81fd3ab0c4a05df232
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-03-18 22:25:53 +0100

    feat: Cloudinary SDK configuration and Class for handling File uploading / deleting

---
+++ src/db/CloudFileHandling.js
@@ -0,0 +1,58 @@
+import { v2 as cloudinary } from "cloudinary";
+import { env } from "process";
+
+cloudinary.config({
+    cloud_name: env.cloud_name,
+    api_key: env.api_key,
+    api_secret: env.api_secret,
+    secure: true,
+});
+
+class CloudFileHandling {
+    async uploadFile(path) {
+        try {
+            const result = await cloudinary.uploader.upload(path, {
+                use_filename: true,
+                unique_filename: true,
+                resource_type: "raw",
+            });
+
+            console.log(result);
+
+            return {
+                cloudPublicId: result.public_id,
+                cloudUrl: result.secure_url,
+            };
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    async deleteFile(publicId) {
+        try {
+            let result = "";
+
+            if (Array.isArray(publicId)) {
+                if (publicId.length > 100) {
+                    // Loop needed for this "case"
+
+                    return;
+                }
+
+                result = await cloudinary.api.delete_resources(publicId, {
+                    resource_type: "raw",
+                });
+            } else {
+                result = await cloudinary.uploader.destroy(publicId, {
+                    resource_type: "raw",
+                });
+            }
+
+            console.log(result);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+}
+
+export let cloudFileHandling = new CloudFileHandling();
