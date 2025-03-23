2477cecd1b92e22f0242ce7c4a8d5c5541d4cb50
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-03-23 13:59:58 +0100

    fix(config): Switch and cases not being formatted correctly

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -28,7 +28,7 @@ export default [
         },
 
         rules: {
-            indent: ["error", 4],
+            indent: ["error", 4, { SwitchCase: 1 }],
             "linebreak-style": ["error", "windows"],
             quotes: ["error", "double"],
             semi: ["error", "always"],
